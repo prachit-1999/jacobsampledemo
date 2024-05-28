a33a729341e14e8bad24ab4ed83f065106566024
prachit-1999 <prachitphansalkar1999@gmail.com>
2024-05-28 18:35:57 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import { Box } from '@mui/material';
 
 function App() {
   return (
-    <Box
-      sx={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '90vh',
-        minWidth: '100vw',
-      }}
-    >
+    <Box>
       <SAMPLE />
     </Box>
   );
